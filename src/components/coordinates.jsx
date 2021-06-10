6623d81df8ba73b85975090f28585f27ffbdcba3
Elijah Harley <heyelijaharley@gmil.com>
2021-06-10 12:22:57 +0100

    bug fixing

--- src/components/coordinates.jsx
+++ src/components/coordinates.jsx
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
 import MapView from 'react-native-maps';
-import Marker from 'react-native-maps'
 
 export default function Coordinates() {
   const [location, setLocation] = useState({
     coords: {
       longitude: 0,
-      latitude: 0,
+      latitude: 0
     }
   });
   const [errorMsg, setErrorMsg] = useState(null);
@@ -35,30 +34,23 @@ export default function Coordinates() {
     console.log(typeof latitude, '<<this is the type of latitude');
     console.log(typeof +latitude, '<<this is the type of latitude with a +');
     text = `Latitude: ${latitude} Longitude: ${longitude}`;
-    
   }
-  
+
   const region = {
-    
     latitude: location.coords.latitude,
     longitude: location.coords.longitude,
     latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  
-}
-
+    longitudeDelta: 0.0421
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>{text}</Text>
-      <MapView 
+      <MapView
         style={{ width: 600, height: 300 }}
         showsUserLocation
         region={region}
-        
-      >
-        
-      </MapView>
+      ></MapView>
     </View>
   );
 }
@@ -69,6 +61,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
